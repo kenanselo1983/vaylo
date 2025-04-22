7b28e65771bd8ac5d0b725ad713ff079c74335ab
kenanselo1983 <97354869+kenanselo1983@users.noreply.github.com>
2025-04-22 09:21:29 +0300

    Update scanner.py

--- backend/scanner.py
+++ backend/scanner.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def fetch_data_from_db(db_path="company_data.db"):
     conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row  # this lets us convert to dicts
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
 
     cursor.execute("SELECT * FROM clients")
