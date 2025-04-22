0d734ee192b967a25f53489db44b74c227e440ed
kenanselo1983 <97354869+kenanselo1983@users.noreply.github.com>
2025-04-22 09:16:59 +0300

    Create scanner.py

---
+++ backend/scanner.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+def fetch_data_from_db(db_path="company_data.db"):
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row  # this lets us convert to dicts
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM clients")
+    rows = cursor.fetchall()
+
+    data = [dict(row) for row in rows]
+    conn.close()
+    return data
